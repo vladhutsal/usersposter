b09c4e170ba4df87b781e332f721152adff04bee
vlhutsal <vl.hutsal@gmail.com>
2021-02-16 15:41:23 +0200

    remove fronted

--- frontend/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <div>
-      <router-link to="/">Posts</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
